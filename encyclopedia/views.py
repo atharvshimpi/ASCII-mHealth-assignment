5687bb8f12a32f07c361e5e65f31f75235b47bc0
atharvshimpi <atharvashimpi@gmail.com>
2022-09-24 22:48:11 +0530

    added entry/errors page

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+import markdown
 
 from . import util
 
+def convertMd2HTML(text):
+    content = util.get_entry(text)
+    markdowner = markdown.Markdown()
+    if content == None:
+        return None
+    else:
+        return markdowner.convert(content)
+    markdowner.convert("*boo!*")
 
 def index(request):
     entries: util.list_entries()
@@ -11,3 +20,14 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entry(request, title):
+    htmlContent = convertMd2HTML(title)
+    if htmlContent == None:
+        return render(request, "encyclopedia/error.html", {
+            "message": "This entry doesn't exist"
+        })
+    else:
+        return render(request, "encyclopedia/entry.html", {
+            "title": title,
+            "content": htmlContent
+        })
